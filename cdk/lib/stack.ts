02dffd3f2b48e2e2254e813d1771e058e2ca1bf2
John <jcwrightson@gmail.com>
2021-07-12 15:39:42 +0100

    add cloudfront

--- cdk/lib/stack.ts
+++ cdk/lib/stack.ts
@@ -2,7 +2,8 @@ import * as cdk from '@aws-cdk/core';
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
 import * as iam from "@aws-cdk/aws-iam";
-
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
 export class ReactStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -11,6 +12,10 @@ export class ReactStack extends cdk.Stack {
       publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       websiteIndexDocument: "index.html",
+    });
+
+    const distrib = new cloudfront.Distribution(this, 'reactAppDist', {
+      defaultBehavior: { origin: new origins.S3Origin(bucket) },
     });
 
     new s3Deployment.BucketDeployment(this, "iw-react-app-hosting-deployment", {
@@ -28,6 +33,10 @@ export class ReactStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, "Bucket URL", {
       value: bucket.bucketDomainName,
+    });
+
+    new cdk.CfnOutput(this, "Cloudfront URL", {
+      value: distrib.distributionDomainName,
     });
   }
 }
